feda0e7eb23f584d3033044750611b1dc225414a
eivindveg <eivindveg@hotmail.com>
2015-02-19 12:05:28 +0100

    tictactoe: expose frame layout as main content frame

--- app/src/main/java/no/westerdals/student/vegeiv13/pg4600/assignment1/tictactoe/app/BaseActivity.java
+++ app/src/main/java/no/westerdals/student/vegeiv13/pg4600/assignment1/tictactoe/app/BaseActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 /**
@@ -14,14 +15,14 @@ public class BaseActivity extends Activity {
 
     protected String[] mListItems;
     protected DrawerLayout mDrawerLayout;
-
     protected ListView mDrawerList;
+    protected FrameLayout mContentFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
-
+        mContentFrame = (FrameLayout) findViewById(R.id.content_frame);
         mListItems = getResources().getStringArray(R.array.drawer_items);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
